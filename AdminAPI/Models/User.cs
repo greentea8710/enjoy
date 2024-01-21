5762d416d4cb9c9ba6e256cf0b12abd10d2b6b63
呂璇 <teapudding8710@gmail.com>
2024-01-21 17:17:40 +0800

    20240121

---
+++ AdminAPI/Models/User.cs
@@ -0,0 +1,53 @@
+﻿namespace AdminAPI.Models
+{
+    public class User
+    {
+
+
+        public short A {  get; set; }
+        public ushort B { get; set; }
+
+
+        public int Id { get; set; }
+        public int? Id1 { get; set; } = null;
+
+        public uint IdX { get; set; }
+
+        public long LongV { get; set; }
+        public ulong LongA { get; set; }
+
+
+        public string Name { get; set; }
+        public string? Namw1 { get; set; }
+
+
+        public bool BoolA { get; set; }
+
+
+        public Guid Gid { get; set; }
+
+
+        public float Fa {get;set; }
+
+
+        public double Dx { get; set; }
+
+
+        public decimal Ds { get; set; }
+
+
+        public DateTime D1 { get; set; }
+
+
+        public DateTimeOffset DateTimeOffset2 { get; set; }
+
+
+        public DateOnly DateOnly { get; set; }
+
+
+        public TimeOnly TimeOnly { get; set; }
+
+
+        public TimeSpan Time { get; set; }
+    }
+}
--- AdminAPI/Program.cs
+++ AdminAPI/Program.cs
@@ -5,6 +5,70 @@ namespace AdminAPI
     {
         public static void Main(string[] args)
         {
+            int i = 1;
+            i = 17 % 2;
+            Console.WriteLine($"int:{i}");
+
+            short j = 3;
+            j = (short)(j +5);
+            Console.WriteLine($"short:{j}");
+
+            ushort k= 2555;
+            k = 3 -3;
+            Console.WriteLine($"ushort:{k}");
+
+
+            long f = 36666666666666;
+            f = f +5 ;
+            Console.WriteLine($"long:{f}");
+
+            string s = "123你好asd";
+            s =s+5 ;
+            s = s + "e";
+            Console.WriteLine($"string:{s}");
+
+
+            double d = 17;
+            d = d / 2;
+            Console.WriteLine($"double:{d}");
+
+            Guid u = Guid.NewGuid();
+            Console.WriteLine($"Guid : {u}");
+
+            DateTime dateTime = DateTime.Now;
+
+            DateOnly dol = DateOnly.FromDateTime(dateTime);
+            Console.WriteLine($"DateOnly{dol}");
+
+            TimeOnly qw = TimeOnly.FromDateTime(dateTime);
+
+      
+
+            Console.WriteLine($"TimeOnly{qw}");
+            DateTime dateTime1 = DateTime.Now.AddMinutes(10);
+            TimeSpan ts = dateTime1 - dateTime;
+
+            ts = TimeSpan.FromHours(70);
+            Console.WriteLine($"TimeSpan{ts}");
+
+            Console.WriteLine($"DateTime : {dateTime.ToString("yyyy年MM月dd日 HH:mm:ss")}");
+
+
+            DateTimeOffset dateTimeOffset = DateTimeOffset.Now;
+            Console.WriteLine($"DataTimeOffset.{dateTimeOffset}");
+
+          
+            bool q = false;
+            q = true;
+            Console.WriteLine($"bool {q}");
+
+
+
+
+
+
+
+
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
